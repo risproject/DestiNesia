6a84fea93c41213ef59c91f662cae25fa35ab849
risproject <faaris.faiz.fadlan@gmail.com>
2024-05-20 14:56:10 +0700

    update ke-3

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+      var navbar = document.querySelector(".navbar");
+      if (window.scrollY > 50) { // Atur jarak scroll sesuai keinginan
+        navbar.classList.add("navbar-backdrop");
+      } else {
+        navbar.classList.remove("navbar-backdrop");
+      }
+    });
+  });
+  
