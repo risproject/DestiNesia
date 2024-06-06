f9553ba4e4fb22c3da9bba75b93bbb85420c44e1
Faaris Muwaffaq <85434419+risproject@users.noreply.github.com>
2024-06-06 14:40:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  window.addEventListener("scroll", function() {
+    window.addEventListener("scroll", function() {
       var navbar = document.querySelector(".navbar");
-      if (window.scrollY > 25) { // Atur jarak scroll sesuai keinginan
+      if (window.scrollY > 50) { // Atur jarak scroll sesuai keinginan
         navbar.classList.add("navbar-backdrop");
       } else {
         navbar.classList.remove("navbar-backdrop");
       }
     });
   });
-
-  
